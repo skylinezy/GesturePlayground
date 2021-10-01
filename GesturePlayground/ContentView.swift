320809c7c0b9b03aef756b3d56f37569e33dec4d
skylinezy <developer.yuanzi@gmail.com>
2021-09-30 23:12:26 -0400

    Initial Commit

---
+++ GesturePlayground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GesturePlayground
+//
+//  Created by Ryan Zi on 9/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GesturePlayground/GesturePlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  GesturePlaygroundApp.swift
+//  GesturePlayground
+//
+//  Created by Ryan Zi on 9/30/21.
+//
+
+import SwiftUI
+
+@main
+struct GesturePlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
